67d1a4009a258985cc3d364a7b0333bd82be2439
Wyatt VO <votranquy96@gmail.com>
2023-10-16 00:23:09 +0700

    Show list product in main page

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 def home(request):
-  context = {}
+  products = Product.objects.all()
+  context = {'products': products}
   return render(request, 'app/home.html', context)
 
 def cart(request):
