d413b564350cc7caee33f6856463f189dbd3faf1
Wyatt VO <votranquy96@gmail.com>
2023-10-15 20:36:04 +0700

    Update url for cart and checkout

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
