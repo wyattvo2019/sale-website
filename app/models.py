e97c53de9dd15c2030af236bb04db8fdad515101
Wyatt VO <votranquy96@gmail.com>
2023-10-16 23:15:46 +0700

    Use real image for product and catch the error product has no image

--- app/models.py
+++ app/models.py
@@ -14,6 +14,13 @@ class Product(models.Model):
   image = models.ImageField(null=True,blank=True)
   def __str__(self):
     return self.name
+  @property
+  def ImageURL(self):
+    try:
+      url = self.image.url
+    except:
+      url = ''
+    return url
   
 class Order(models.Model):
   customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
